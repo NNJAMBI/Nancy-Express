01af1a9bf60005280000844d1c77d49778f0de0a
Nancy Karanja <nnjambi@safaricom.co.ke>
2020-07-14 01:35:32 +0300

    add changes made to the project

---
+++ app.js
@@ -0,0 +1,35 @@
+// Import express package
+const express = require('express');
+const upload = require('./upload');
+
+// Initialize express
+const app = express();
+
+// Set up a view engine
+app.set('view engine', 'ejs');
+
+// Set a static folder
+app.use(express.static('public'));
+
+app.get('/', (req,res)=>{
+   res.render('index');
+})
+
+// route to handle image upload
+app.post('/upload', (req,res)=>{
+   upload(req,res, (err)=>{
+       if (err){
+           console.log(err)
+       }else{
+           console.log(req.file)
+           res.send('test');
+       }
+   })
+})
+
+// Define the port number
+const PORT = 5000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on port ${PORT}`)
+});
