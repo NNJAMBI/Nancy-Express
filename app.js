b88f860fc650f96aabe9a4a80596f89f578adb29
Nancy Karanja <nnjambi@safaricom.co.ke>
2020-07-15 00:46:38 +0300

    add changes made to all files

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Import express package
 const express = require('express');
-const upload = require('./upload');
+
+let indexRouter = require('./routes/index');
 
 // Initialize express
 const app = express();
@@ -8,28 +9,17 @@ const app = express();
 // Set up a view engine
 app.set('view engine', 'ejs');
 
+
 // Set a static folder
 app.use(express.static('public'));
 
-app.get('/', (req,res)=>{
-   res.render('index');
-})
+// Define the index router
+app.use('/', indexRouter);
 
-// route to handle image upload
-app.post('/upload', (req,res)=>{
-   upload(req,res, (err)=>{
-       if (err){
-           console.log(err)
-       }else{
-           console.log(req.file)
-           res.send('test');
-       }
-   })
-})
 
 // Define the port number
 const PORT = 5000;
 
 app.listen(PORT, ()=>{
-    console.log(`Server is listening on port ${PORT}`)
+   console.log(`Server is listening on port ${PORT}`)
 });
