09ef3abaebb17aaf811e1949ba86c862ea849bf7
Nancy Karanja <nnjambi@safaricom.co.ke>
2020-07-18 00:09:05 +0300

    add changes made to all files

--- app.js
+++ app.js
@@ -1,9 +1,76 @@
-// Import express package
+// // Imports
+// const express = require('express');
+// const mongoose = require('mongoose');
+
+// let indexRouter = require('./routes/index');
+// let imageRouter = require('./routes/image');
+
+// // Connecting to the Database
+// let mongodb_url = 'mongodb://localhost/';
+// let dbName = 'darkroom';
+// mongoose.connect(mongodb_url + dbName,)
+// let db = mongoose.connection;
+
+// // Check Connection
+// db.once('open', ()=>{
+//    console.log('Database connected successfully')
+// })
+
+// // Check for DB Errors
+// db.on('error', (error)=>{
+//    console.log(error);
+// })
+
+// // Initialize express
+// const app = express();
+
+// // Set up a view engine
+// app.set('view engine', 'ejs');
+
+
+// // Set a static folder
+// app.use(express.static('public'));
+
+// // body parser middleware
+// app.use(express.json())
+
+// // Define the index router
+// app.use('/', indexRouter);
+// app.use('/image', imageRouter);
+
+
+// // Define the port number
+// const PORT = 5000;
+
+// app.listen(PORT, ()=>{
+//    console.log(`Server is listening on port ${PORT}`)
+// });
+
+// Imports
 const express = require('express');
+const mongoose = require('mongoose');
 
 let indexRouter = require('./routes/index');
+let imageRouter = require('./routes/image');
 
-// Initialize express
+// Connecting to the Database
+let mongodb_url = 'mongodb://localhost/';
+let dbName = 'darkroom';
+mongoose.connect(mongodb_url + dbName,)
+let db = mongoose.connection;
+
+// Check Connection
+db.once('open', ()=>{
+    console.log('Database connected successfully')
+})
+
+// Check for DB Errors
+db.on('error', (error)=>{
+    console.log(error);
+})
+
+
+// Initiliaze express
 const app = express();
 
 // Set up a view engine
@@ -13,13 +80,17 @@ app.set('view engine', 'ejs');
 // Set a static folder
 app.use(express.static('public'));
 
-// Define the index router
+// body parser middleware
+app.use(express.json())
+
+// Routes
 app.use('/', indexRouter);
+app.use('/image', imageRouter);
 
 
 // Define the port number
 const PORT = 5000;
 
 app.listen(PORT, ()=>{
-   console.log(`Server is listening on port ${PORT}`)
+    console.log(`Server is listening on port ${PORT}`)
 });
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,48 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../upload');
+const Photo = require('../models/photos');
 
+// Home page router
 router.get('/', (req,res)=>{
-   res.render('index');
-})
+    Photo.find({}, (error, images)=>{
+        if (error){
+            console.log(error);
+        }else{
+ 
+            res.render('index', { images:images, msg: req.query.msg});
+        }
+    })
+ })
+// router.get('/', (req,res)=>{
+//     res.render('index', {msg: req.query.msg});
+//  })
+ 
 
-// route to handle image upload
-router.post('/upload', (req,res)=>{
-   upload(req, res, (err)=>{
-       if (err){
-           console.log(err)
-           res.render('index', {msg: err})
-       }else{
-           console.log(req.file);
-           res.render('index', {file: 'images/' + req.file.filename})
-       }
-   })
-})
-
+ // route to handle image upload
+ router.post('/upload', (req,res)=>{
+    upload(req, res, (err)=>{
+        if (err){
+            console.log(err)
+            // Redirect to the index page and pass a msg to the template
+            res.redirect(`/?msg=${err}`);
+        }else if(req.file === undefined){
+            // Redirect to the index page and pass a msg to the template
+            res.redirect('/?msg=Error: No file selected!');
+        }else{
+            // Create a new photo
+            let newPhoto = new Photo({
+                name: req.file.filename,
+                path: 'images/' + req.file.filename,
+                size: req.file.size
+            })
+ 
+            // Save newPhoto in the database
+            newPhoto.save()
+ 
+            // Redirect to the index page and pass a msg to the template
+            res.redirect('/?msg=File uploaded successfully');
+        }
+    })
+ })
 module.exports = router;
