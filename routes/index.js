2d96356126bd71dd8a3376ecb08480ebcf2b0dad
Nancy Karanja <nnjambi@safaricom.co.ke>
2020-07-20 17:17:43 +0300

    add changes to the script.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const upload = require('../upload');
 const Photo = require('../Models/photos');
-
 // Home page router
 router.get('/', (req,res)=>{
     Photo.find({}, (error, images)=>{
